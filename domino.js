2e7334ec8dfd5d0cf172ffe30e78a6d504b23464
Rúben Domingues <51157774+rubendomingues@users.noreply.github.com>
2019-11-02 16:01:14 +0000

    Add files via upload

--- domino.js
+++ domino.js
@@ -19,17 +19,63 @@ function rulesButton(){
   span.onclick = function() {
     modal.style.display = "none";
   }
+}
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+//Function to present a modal box with the scores
+function scoresButton(){
+
+  // Get the modal
+  var modal = document.getElementById("myScores");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("scores");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[1];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
   }
 }
 
-//Function hidde login when logged and make game options appear
-function login(){
+function logoutFunc(){
+  var menu = document.getElementById("playMenu");
+
+  var logoutmenu = document.getElementById("logout");
+
+  var login = document.getElementById("loginButton");
+
+  var logout = document.getElementById("buttonLogout");
+
+  var elem = document.getElementById('loginContainer');
+
+  var left = document.getElementById("leftContainer");
+
+  var gameBoard = document.getElementById("gameBoard");
+
+  var giveUp = document.getElementById("forfeit");
+
+  var game = document.getElementById("board2");
+
+  game.style.display = "none";
+  menu.style.display = "none";
+  logoutmenu.style.display = "none";
+  elem.style.display = "block";
+  left.style.display = "block";
+  gameBoard.style.display = "none";
+  giveUp.style.display = "none";
+
+  document.getElementById("left").style.visibility = "hidden";
+  document.getElementById("right").style.visibility = "hidden";
+
+}
+function loginFunc(){
 
   var menu = document.getElementById("playMenu");
 
@@ -47,21 +93,19 @@ function login(){
 
   var giveUp = document.getElementById("forfeit");
 
+  var game = document.getElementById("board2");
 
-  login.onclick = function() {
+    var user = document.getElementById("user");
+    var pass = document.getElementById("pass")
+    if(user.value=="" || pass.value=="")
+      window.alert("Insert username and password before login!");
+    else{
+      document.getElementById("userLogout").innerHTML = "Welcome, " + user.value;
+
     elem.style.display = "none";
     menu.style.display = "block";
     logoutmenu.style.display = "block";
     return false;
-  }
-
-  logout.onclick = function() {
-    menu.style.display = "none";
-    logoutmenu.style.display = "none";
-    elem.style.display = "block";
-    left.style.display = "block";
-    gameBoard.style.display = "none";
-    giveUp.style.display = "none";
   }
 
 }
@@ -69,25 +113,15 @@ function login(){
 function disappear(){
   var left = document.getElementById("leftContainer");
 
-  var human = document.getElementById("button1x1");
-
   var gameBoard = document.getElementById("gameBoard");
 
   var computer = document.getElementById("button1xpc");
 
-  var giveUp = document.getElementById("forfeit");
+  var dif = document.getElementById("Dificulty");
 
-  human.onclick = function(){
-    left.style.display = "none";
-    gameBoard.style.display = "block";
-    giveUp.style.display = "block";
-  }
-
-  computer.onclick = function(){
-    left.style.display = "none";
-    gameBoard.style.display = "block";
-    giveUp.style.display = "block";
-  }
+  dif.style.display = "block";
+  left.style.display = "none";
+  gameBoard.style.display = "block";
 
 }
 
@@ -104,11 +138,15 @@ function giveGame(){
 
   var gameBoard = document.getElementById("gameBoard");
 
-    btn.onclick = function(){
-      left.style.display = "block";
-      giveUp.style.display = "none";
-      menu.style.display = "block";
-      logoutmenu.style.display = "block";
-      gameBoard.style.display = "none";
-    }
+  var board2 = document.getElementById("board2");
+
+  left.style.display = "block";
+  giveUp.style.display = "none";
+  menu.style.display = "block";
+  logoutmenu.style.display = "block";
+  gameBoard.style.display = "none";
+  board2.style.display = "none";
+
+  document.getElementById("left").style.visibility = "hidden";
+  document.getElementById("right").style.visibility = "hidden";
 }
